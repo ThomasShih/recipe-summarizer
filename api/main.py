e77667853c1402d98e02afa9cb245d3e20510482
Thomas Shih <55861476+ThomasShih@users.noreply.github.com>
2023-10-25 08:41:19 -0600

    Added langchain

--- api/main.py
+++ api/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from lib import summarize_recipe, extract_from_url
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Link(BaseModel):
+    url: str
+
 @app.post("/")
-async def root_post():
-    return {"message": "Hello World"}
+async def root_post(link: Link):
+    page_contents = extract_from_url(link.url)
+    return {
+        "message": summarize_recipe(page_contents)
+    }
--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 import uvicorn
+from dotenv import load_dotenv
+
+load_dotenv()
 from api import app
 
 if __name__ == "__main__":
